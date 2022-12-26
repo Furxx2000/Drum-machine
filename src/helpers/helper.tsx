39d7a18adab5a153b2892b70b537a485f3d5222b
furxx2000 <furxx2000@gmail.com>
2022-12-26 23:30:16 +0800

    Add helper func

---
+++ src/helpers/helper.tsx
@@ -0,0 +1,57 @@
+import Heater1 from '../assets/Heater-1.mp3';
+import Heater2 from '../assets/Heater-2.mp3';
+import Heater3 from '../assets/Heater-3.mp3';
+import Heater4 from '../assets/Heater-4.mp3';
+import Clap from '../assets/Heater-6.mp3';
+import OpenHH from '../assets/Dsc_Oh.mp3';
+import KickNHat from '../assets/Kick_n_Hat.mp3';
+import Kick from '../assets/RP4_KICK_1.mp3';
+import ClosedHH from '../assets/Cev_H2.mp3';
+
+export const pads = [
+  {
+    id: 'Heater 1',
+    text: 'Q',
+    src: Heater1,
+  },
+  {
+    id: 'Heater 2',
+    text: 'W',
+    src: Heater2,
+  },
+  {
+    id: 'Heater 3',
+    text: 'E',
+    src: Heater3,
+  },
+  {
+    id: 'Heater 4',
+    text: 'A',
+    src: Heater4,
+  },
+  {
+    id: 'Clap',
+    text: 'S',
+    src: Clap,
+  },
+  {
+    id: 'Open-HH',
+    text: 'D',
+    src: OpenHH,
+  },
+  {
+    id: "Kick-n'-H",
+    text: 'Z',
+    src: KickNHat,
+  },
+  {
+    id: 'Kick',
+    text: 'X',
+    src: Kick,
+  },
+  {
+    id: 'Closed-HH',
+    text: 'C',
+    src: ClosedHH,
+  },
+];
