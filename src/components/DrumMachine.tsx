89fc6d56341ea2cabf1cafb964e08fa6962b3695
Danny Wen <danny_wen@trendmicro.com>
2022-12-27 18:32:38 +0800

    Get data by useDrumState()

--- src/components/DrumMachine.tsx
+++ src/components/DrumMachine.tsx
@@ -1,14 +1,16 @@
 import { Container } from './styles/Container.styled';
 import { DrumPanel } from './styles/DrumPanel.styled';
 import { DrumPad } from './DrumPad';
+import { ControlPanel } from './ControlPanel';
 import { pads } from '../helpers/helper';
 
 export default function DrumMachine() {
   return (
     <Container id='drum-machine'>
+      <ControlPanel />
       <DrumPanel>
         {pads.map((pad) => (
-          <DrumPad key={pad.text} id={pad.id} text={pad.text} src={pad?.src} />
+          <DrumPad key={pad.text} pad={pad} />
         ))}
       </DrumPanel>
     </Container>
--- src/components/DrumPad.tsx
+++ src/components/DrumPad.tsx
@@ -1,25 +1,42 @@
-import { StyledDrumPad } from './styles/StyledDrumPad.styled';
+import { StyledDrumPad } from './styles/DrumPad.styled';
+import { useDrumState } from '../hooks/useDrumState';
 
 interface Props {
-  id: string;
-  text: string;
-  src: any;
+  pad: {
+    id: string;
+    text: string;
+    src: any;
+  };
 }
 
-export function DrumPad({ id, text, src }: Props) {
-  async function handleClick() {
+export function DrumPad({ pad: { id, text, src } }: Props) {
+  const { volume, power, handleSetKey } = useDrumState();
+
+  async function handleClickKey(targetButton: HTMLButtonElement) {
     const audio = document.getElementById(text) as HTMLAudioElement;
+    const keyName = id.replaceAll('-', ' ');
     try {
+      targetButton?.classList.add('is-active');
+      setTimeout(() => {
+        targetButton?.classList.remove('is-active');
+      }, 50);
+      if (!power) return;
+      audio.volume = volume;
       audio.currentTime = 0;
       await audio?.play();
     } catch (err) {
       throw err;
     }
+    handleSetKey(keyName);
   }
 
   return (
-    <StyledDrumPad className={`drum-pad ${id}`} onClick={handleClick}>
-      <audio id={text} src={src}></audio>
+    <StyledDrumPad
+      id={id}
+      className='drum-pad'
+      onClick={(e) => handleClickKey(e.currentTarget)}
+    >
+      <audio id={text} className='clip' src={src}></audio>
       {text}
     </StyledDrumPad>
   );
