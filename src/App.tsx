557fb56529cebf211de36a631d59b6a5d67cfa49
Danny Wen <danny_wen@trendmicro.com>
2022-12-27 18:30:36 +0800

    Use provider to pass props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import DrumMachine from './components/DrumMachine';
 import { GlobalStyle } from './components/styles/GlobalStyle.styled';
+import { DrumStateProvider } from './hooks/useDrumState';
 
 export default function App() {
   return (
-    <>
+    <DrumStateProvider>
       <DrumMachine />
       <GlobalStyle />
-    </>
+    </DrumStateProvider>
   );
 }
